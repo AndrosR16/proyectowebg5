1e2565cf56ecdffb015526acc903205c25fe7f93
AndrosR16 <123216631+AndrosR16@users.noreply.github.com>
2026-07-21 12:04:43 -0600

    feat: crear estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/fide/proyectowebg5/Proyectowebg5Application.java
@@ -0,0 +1,13 @@
+package com.fide.proyectowebg5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectowebg5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectowebg5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fide/proyectowebg5/Proyectowebg5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fide.proyectowebg5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectowebg5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
