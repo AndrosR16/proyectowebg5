9a610966149ff8ab0631c5af559db0e70666fd89
Santiago <ramirezsantiagobejarano@gmail.com>
2026-07-26 20:58:01 -0600

    Login completo con sesión y cierre de sesión

--- src/main/java/com/fide/proyectowebg5/controller/HomeController.java
+++ src/main/java/com/fide/proyectowebg5/controller/HomeController.java
@@ -47,4 +47,13 @@ public class HomeController {
         return "index";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+
+        session.invalidate();
+
+        return "redirect:/";
+
+    }
+
 }
